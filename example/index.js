52007db12265580775066101926c605453336a4b
Matthias Van Parijs <matvp91@gmail.com>
2020-12-13 19:01:05 +0100

    Added chromeless parameter, when set to true, the UI will not be initialized.

--- example/index.js
+++ example/index.js
@@ -5,20 +5,30 @@ import ShakaPlayer from 'shaka-player-react';
 const STREAMS = [
   {
     name: 'Angel One MPEG-DASH',
-    src: 'https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd',
+    src: 'https://storage.googleapis.com/shaka-demo-assets/angel-one/dash.mpd'
   },
   {
     name: 'Big Buck Bunny HLS',
     src:
-      'https://storage.googleapis.com/shaka-demo-assets/bbb-dark-truths-hls/hls.m3u8',
-  },
+      'https://storage.googleapis.com/shaka-demo-assets/bbb-dark-truths-hls/hls.m3u8'
+  }
 ];
 
 function App() {
   const [show, setShow] = React.useState(false);
+  const [chromeless, setChromeless] = React.useState(false);
+  const ref = React.useRef(null);
+
+  React.useEffect(() => {
+    window.getShakaInst = () => ref.current;
+  }, []);
 
   function onToggle() {
     setShow(!show);
+  }
+
+  function onChromeless() {
+    setChromeless(!chromeless);
   }
 
   const [src, setSrc] = React.useState(STREAMS[0].src);
@@ -33,13 +43,18 @@ function App() {
         <button onClick={onToggle}>{show ? 'Hide' : 'Show'}</button>
       </div>
       <div>
+        <input type="checkbox" onChange={onChromeless} /> Chromeless
+      </div>
+      <div>
         <select value={src} onChange={onSelectSrc}>
           {STREAMS.map(stream => (
             <option value={stream.src}>{stream.name}</option>
           ))}
         </select>
       </div>
-      {show && <ShakaPlayer autoPlay src={src} />}
+      {show && (
+        <ShakaPlayer ref={ref} autoPlay src={src} chromeless={chromeless} />
+      )}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -12,68 +12,78 @@ import React from 'react';
  * @returns {*}
  * @constructor
  */
-function ShakaPlayer({ src, config, autoPlay, width, height }, ref) {
+function ShakaPlayer({ src, config, chromeless, className, ...rest }, ref) {
   const uiContainerRef = React.useRef(null);
   const videoRef = React.useRef(null);
-  const controller = React.useRef({});
+
+  const [player, setPlayer] = React.useState(null);
+  const [ui, setUi] = React.useState(null);
 
   // Effect to handle component mount & mount.
   // Not related to the src prop, this hook creates a shaka.Player instance.
   // This should always be the first effect to run.
   React.useEffect(() => {
     const player = new shaka.Player(videoRef.current);
-    const ui = new shaka.ui.Overlay(
-      player,
-      uiContainerRef.current,
-      videoRef.current,
-    );
+    setPlayer(player);
 
-    // Store Shaka's API in order to expose it as a handle.
-    controller.current = { player, ui, videoElement: videoRef.current };
+    let ui;
+    if (!chromeless) {
+      const ui = new shaka.ui.Overlay(
+        player,
+        uiContainerRef.current,
+        videoRef.current
+      );
+      setUi(ui);
+    }
 
     return () => {
       player.destroy();
-      ui.destroy();
+      if (ui) {
+        ui.destroy();
+      }
     };
   }, []);
 
   // Keep shaka.Player.configure in sync.
   React.useEffect(() => {
-    const { player } = controller.current;
     if (player && config) {
       player.configure(config);
     }
-  }, [config]);
+  }, [player, config]);
 
   // Load the source url when we have one.
   React.useEffect(() => {
-    const { player } = controller.current;
-    if (player) {
+    if (player && src) {
       player.load(src);
     }
-  }, [src]);
+  }, [player, src]);
 
   // Define a handle for easily referencing Shaka's player & ui API's.
-  React.useImperativeHandle(ref, () => ({
-    get player() {
-      return controller.current.player;
-    },
-    get ui() {
-      return controller.current.ui;
-    },
-    get videoElement() {
-      return controller.current.videoElement;
-    },
-  }));
+  React.useImperativeHandle(
+    ref,
+    () => ({
+      get player() {
+        return player;
+      },
+      get ui() {
+        return ui;
+      },
+      get videoElement() {
+        return videoRef.current;
+      }
+    }),
+    [player, ui]
+  );
 
   return (
-    <div ref={uiContainerRef}>
+    <div ref={uiContainerRef} className={className}>
       <video
         ref={videoRef}
-        autoPlay={autoPlay}
-        width={width}
-        height={height}
-        style={{ maxWidth: '100%' }}
+        style={{
+          maxWidth: '100%',
+          width: '100%'
+        }}
+        {...rest}
       />
     </div>
   );
